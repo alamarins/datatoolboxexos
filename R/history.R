b17ba5b2590e5c622aeed01430648f74b2e84ac3
Amaïa Lamarins <amaia.lamarins@laposte.net>
2020-11-03 16:21:42 +0100

    :beer: rmarkdown normally using functions from package compendium

--- R/history.R
+++ R/history.R
@@ -3,3 +3,7 @@ usethis::use_r("data_wildfinder")
 usethis::use_package("here")
 usethis::use_package("readr")
 rrtools::use_readme_rmd()
+devtools::install_deps() ##installe tous package dans imports et dependencies du fichier description
+devtools::document() ##créé la documentation à partir des trucs roxygen de nos fonctions, créé dossier man
+#quand je fais ca, me demande d'écraser le fichier description et supprime le contenu du fichier data_wildfinder où y a les fonctions
+devtools::load_all() #rend toutes les fonctions disponibles mais ne les place pas dans l'environnement de travail comme avec source
